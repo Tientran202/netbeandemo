1bae1c12f0bbf17ee5e107c7037c8ecbe6fcc2cb
Tientran202 <tranvuongngoctien@gmail.com>
2022-08-04 09:55:56 +0700

    test netbean

---
+++ src/main/java/helloWord.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+import java.sql.Connection;
+
+public class helloWord {
+    public static void main(String[] args) {
+        String ten = "Anh dành tất cả thanh xuân cho em";
+        String chuoi1 = "chúng ta của hiện tại";
+        for (int i = 10;i <= 20; i++) {
+            System.out.println(" Nhớ ae quá!!");
+            System.out.println(ten);
+            System.out.println("chúng ta");
+        }
+
+    }
+}
